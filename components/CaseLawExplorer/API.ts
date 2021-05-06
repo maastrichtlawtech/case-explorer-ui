0c4c976fe64483ed6a471c30bf8a0cc81d582353
Bogdan Covrig <bogcov@gmail.com>
2021-05-06 23:08:36 +0200

    Add QueryNetworkByUserInput API calls

--- components/CaseLawExplorer/API.ts
+++ components/CaseLawExplorer/API.ts
@@ -13,16 +13,51 @@ const API_AUTH_MODE = {
   API_KEY: 'API_KEY'
 } as const
 
-const LIST_CASES = `query ListCases {
-  listCaselaws {
-    items {
-      abstract
-      country
-      court
-      date
-      doctype
+const convertJSONStringFields = (item) => {
+  return {
+    ...item,
+    ...(item.position ? { position: JSON.parse(item.position)} : {}),
+    data: JSON.parse(item.data)
+  }
+}
+
+const LIST_CASES = `query QueryNetworkByUserInput(
+  $DataSources: [String]
+  $Keywords: String
+  $Articles: String
+  $Eclis: String
+  $DegreesSources: Int
+  $DegreesTargets: Int
+  $DateStart: AWSDate
+  $DateEnd: AWSDate
+  $Instances: [String]
+  $Domains: [String]
+  $Doctypes: [String]
+  $LiPermission: Boolean
+) {
+  queryNetworkByUserInput(
+    DataSources: $DataSources
+    Keywords: $Keywords
+    Articles: $Articles
+    Eclis: $Eclis
+    DegreesSources: $DegreesSources
+    DegreesTargets: $DegreesTargets
+    DateStart: $DateStart
+    DateEnd: $DateEnd
+    Instances: $Instances
+    Domains: $Domains
+    Doctypes: $Doctypes
+    LiPermission: $LiPermission
+  ) {
+    nodes {
       id
-      subject
+      data
+    }
+    edges {
+      id
+      source
+      target
+      data
     }
   }
 }`
@@ -34,15 +69,27 @@ const GET_ELEMENT_DATA = `query GetElementData($id: String) {
   }
 }`
 
-export async function listCases() {
+type listCasesVariables = {
+  DataSources: string[];
+}
+
+export async function listCases(variables: listCasesVariables) {
   try {
+    console.log(variables)
     const listCasesResult = await API.graphql({
       query: LIST_CASES,
       authMode: API_AUTH_MODE.API_KEY,
-      // variables
+      variables
     })
-    const caseResults = listCasesResult.data.listCaselaws.items
-    return caseResults
+
+    const caseResults = listCasesResult.data.queryNetworkByUserInput
+    console.log(caseResults)
+    
+    return {
+      nodes: caseResults.nodes.map(convertJSONStringFields),
+      // edges: project.edges.items.map(convertJSONStringFields),
+    }
+    
     // return caseResults.map(project => ({
     //   // ...project,
     //   nodes: project.nodes.items.map(convertJSONStringFields),
@@ -85,8 +132,6 @@ export async function complexQuery(query: any) {
     console.log('error creating node:', err)
   }
 }
-
-
 
 type GetElementDataVariables = {
   id: string;
