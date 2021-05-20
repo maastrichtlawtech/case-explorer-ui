19b64e97f7b84c04e559abde69ba544aeb49902d
sabaturgay <turgaysaba@outlook.com>
2021-05-20 13:28:39 +0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -2,11 +2,17 @@ import React from 'react';
 import GraphEditor from "./components/CaseLawExplorer";
 import Amplify  from "aws-amplify";
 import { Button }  from "@material-ui/core";
-import { withAuthenticator } from '@aws-amplify/ui-react';
+import { withAuthenticator  } from '@aws-amplify/ui-react';
 import * as API from './components/CaseLawExplorer/API';
-import awsconfig from './src/aws-exports';
+// import awsconfig from './src/aws-exports';
 
-Amplify.configure(awsconfig);
+Amplify.configure({
+  "aws_project_region": "eu-central-1",
+  "aws_appsync_graphqlEndpoint": "https://culpdi4smbeqtjyiqaqxusuv3q.appsync-api.eu-central-1.amazonaws.com/graphql",
+  "aws_appsync_region": "eu-central-1",
+  "aws_appsync_authenticationType": "API_KEY",
+  "aws_appsync_apiKey": "da2-l7smc55gkvgbdftblcbfra4d5y"
+});
 
 
 // runQuery()
@@ -18,15 +24,6 @@ const runQuery = async ()=> {
 }
 
 const App = () => {
-  // React.useEffect(() => {
-  //   const runQuery = async ()=> {
-  //     const result = await API.complexQuery({
-    
-  //     })
-  //     console.log('API RESULT: ', result)
-  //   }
-  //   runQuery()
-  // }, [])
   return (
     <div>
       {/* <AmplifySignOut /> */}
@@ -39,4 +36,8 @@ const App = () => {
   )
  }
 
-export default App//withAuthenticator(App);
+export default withAuthenticator(App, {
+  federated: {
+    googleClientId: '678991149519-fhmj5s0vl3tt1ic9elps5l47s4fdtcb3.apps.googleusercontent.com',
+  }
+});
