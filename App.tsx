6dc3ec0f0ec87d4774a159c8c9e31645e4c977a3
sabaturgay <turgaysaba@outlook.com>
2021-05-21 14:06:41 +0300

    fix authentication

--- App.tsx
+++ App.tsx
@@ -4,19 +4,10 @@ import Amplify  from "aws-amplify";
 import { Button }  from "@material-ui/core";
 import { withAuthenticator  } from '@aws-amplify/ui-react';
 import * as API from './components/CaseLawExplorer/API';
-// import awsconfig from './src/aws-exports';
+import awsconfig from './src/aws-exports';
 
-// Amplify.configure(awsconfig);
-Amplify.configure({
-  "aws_project_region": "eu-central-1",
-  "aws_appsync_graphqlEndpoint": "https://culpdi4smbeqtjyiqaqxusuv3q.appsync-api.eu-central-1.amazonaws.com/graphql",
-  "aws_appsync_region": "eu-central-1",
-  "aws_appsync_authenticationType": "API_KEY",
-  "aws_appsync_apiKey": "da2-l7smc55gkvgbdftblcbfra4d5y"
-});
+Amplify.configure(awsconfig);
 
-
-// runQuery()
 const runQuery = async ()=> {
   const result = await API.testAuth({
     "id": "ECLI:NL:HR:2012:BV5128"
@@ -27,18 +18,30 @@ const runQuery = async ()=> {
 const App = () => {
   return (
     <div>
-      {/* <AmplifySignOut /> */}
       <Button
-        // variant="contained"
         onClick={runQuery}
       >Test the API</Button>
       <GraphEditor />
     </div>
   )
- }
+}
 
-export default withAuthenticator(App, {
-  federated: {
-    googleClientId: '678991149519-fhmj5s0vl3tt1ic9elps5l47s4fdtcb3.apps.googleusercontent.com',
-  }
-});
+const AppContent = withAuthenticator(App);
+
+const AppContainer = () => {
+  React.useEffect(() => {
+    setTimeout(()=>{
+      const button = document.getElementsByTagName('amplify-authenticator')[0]
+    .shadowRoot?.children[0].getElementsByTagName('amplify-sign-in')[0]
+    .shadowRoot?.children[0].getElementsByTagName('amplify-federated-buttons')[0]
+    .shadowRoot?.children[0].getElementsByTagName('amplify-oauth-button')[0]
+    .shadowRoot?.children[0].getElementsByTagName('button')[0]
+    .click()
+    }, 400 )
+  }, [])
+  return (
+    <AppContent/>
+  )
+}
+
+export default AppContainer
