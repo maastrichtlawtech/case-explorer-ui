393624c7ce76bdb277e6eae1faa8938ab60701e6
sabaturgay <turgaysaba@outlook.com>
2021-05-31 14:31:35 +0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -8,8 +8,34 @@ import { withAuthenticator, AmplifyAuthenticator, AmplifySignIn,
     } from '@aws-amplify/ui-react';
   import { AuthState, onAuthUIStateChange } from '@aws-amplify/ui-components'
 import * as API from './components/CaseLawExplorer/API';
-import awsconfig from './src/aws-exports';
+// import awsconfig from './src/aws-exports';
 import { Auth } from 'aws-amplify';
+
+const awsconfig = {
+  "aws_project_region": "eu-central-1",
+  "aws_appsync_graphqlEndpoint": "https://culpdi4smbeqtjyiqaqxusuv3q.appsync-api.eu-central-1.amazonaws.com/graphql",
+  "aws_appsync_region": "eu-central-1",
+  "aws_appsync_authenticationType": "AMAZON_COGNITO_USER_POOLS",
+  "aws_appsync_apiKey": "da2-l7smc55gkvgbdftblcbfra4d5y",
+  "aws_cognito_identity_pool_id": "eu-central-1:d0d334ac-2447-4b56-9467-24eb22b28144",
+  "aws_cognito_region": "eu-central-1",
+  "aws_user_pools_id": "eu-central-1_czNZx5VZq",
+  "aws_user_pools_web_client_id": "1im211cm81rp1sqinrlfq06bp8",
+  "oauth": {
+      "domain": "caseexplorerui357be15f-357be15f-dev.auth.eu-central-1.amazoncognito.com",
+      "scope": [
+          "phone",
+          "email",
+          "openid",
+          "profile",
+          "aws.cognito.signin.user.admin"
+      ],
+      "redirectSignIn": "http://localhost:19006/",
+      "redirectSignOut": "http://localhost:19006/",
+      "responseType": "code"
+  },
+  "federationTarget": "COGNITO_USER_AND_IDENTITY_POOLS"
+}
 
 Amplify.configure(awsconfig);
 
