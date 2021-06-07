8f346137c06f21ba61369a89bc43409844e422b3
sabaturgay <turgaysaba@outlook.com>
2021-06-07 11:34:23 +0300

    add TermsOfService modal and useAuthState

--- App.tsx
+++ App.tsx
@@ -1,17 +1,19 @@
+import { AuthState, onAuthUIStateChange, } from '@aws-amplify/ui-components';
+import { AmplifyAuthenticator,  } from '@aws-amplify/ui-react';
+import { Button } from "@material-ui/core";
+import Amplify  from "aws-amplify";
 import React from 'react';
-import GraphEditor, { 
+import GraphEditor, {
   ACTIONS
 } from "./components/CaseLawExplorer";
-import Amplify  from "aws-amplify";
-import { Button, CircularProgress }  from "@material-ui/core";
-import { withAuthenticator, AmplifyAuthenticator, AmplifySignIn,
-    } from '@aws-amplify/ui-react';
-  import { AuthState, onAuthUIStateChange } from '@aws-amplify/ui-components'
 import * as API from './components/CaseLawExplorer/API';
-// import awsconfig from './src/aws-exports';
-import { Auth } from 'aws-amplify';
+import * as R from 'colay/ramda';
+import {
+  CircularProgress
+} from '@material-ui/core';
+import { TermsOfService } from './components/TermsOfService';
 
-const awsconfig = {
+const AWS_PROD_CONFIG = {
   "aws_project_region": "eu-central-1",
   "aws_appsync_graphqlEndpoint": "https://culpdi4smbeqtjyiqaqxusuv3q.appsync-api.eu-central-1.amazonaws.com/graphql",
   "aws_appsync_region": "eu-central-1",
@@ -30,17 +32,28 @@ const awsconfig = {
           "profile",
           "aws.cognito.signin.user.admin"
       ],
-      // "redirectSignIn": "http://localhost:19006/",
-      // "redirectSignOut": "http://localhost:19006/",
-      "redirectSignIn": "https://dev.dbvgwmy1tjq9d.amplifyapp.com/", 
-      "redirectSignOut": "https://dev.dbvgwmy1tjq9d.amplifyapp.com/",
+      // "redirectSignIn": "https://dev.dbvgwmy1tjq9d.amplifyapp.com/", 
+      // "redirectSignOut": "https://dev.dbvgwmy1tjq9d.amplifyapp.com/",
+      "redirectSignIn": "http://localhost:19006/",
+      "redirectSignOut": "http://localhost:19006/",
       "responseType": "code"
   },
   "federationTarget": "COGNITO_USER_POOLS"
-};
+}
+const AWS_DEV_CONFIG_OVERRIDE = {
+  "oauth": {
+      "redirectSignIn": "http://localhost:19006/",
+      "redirectSignOut": "http://localhost:19006/",
+  },
+}
 
+const AWS_CONFIG = AWS_PROD_CONFIG
+// __DEV__
+//   ? R.mergeDeepRight(AWS_PROD_CONFIG, AWS_DEV_CONFIG_OVERRIDE)
+//   : AWS_PROD_CONFIG
 
-Amplify.configure(awsconfig);
+console.log(AWS_CONFIG)
+Amplify.configure(AWS_CONFIG);
 
 const runQuery = async ()=> {
   const result = await API.testAuth({
@@ -69,29 +82,43 @@ const App = () => {
   )
 }
 
-// const AppWithAuth = () => {
-//   const [authState, setAuthState] = React.useState();
-//   const [user, setUser] = React.useState();
+const AppWithAuth = () => {
+  const [authState, setAuthState] = React.useState();
+  const [user, setUser] = React.useState();
 
-//     React.useEffect(() => {
-//         return onAuthUIStateChange((nextAuthState, authData) => {
-//             setAuthState(nextAuthState);
-//             setUser(authData)
-//         });
-//     }, []);
-
-//   return authState === AuthState.SignedIn && user ? (
-//       <App />
-//     ) : (
-//       <AmplifyAuthenticator>
-//         <AmplifySignIn
-//           // headerText="My Custom Sign In Text"
-//           slot="sign-in"
-//         ></AmplifySignIn>
-//       </AmplifyAuthenticator>
-//   );
-// }
-const AppWithAuth = withAuthenticator(App);
+    React.useEffect(() => {
+      return onAuthUIStateChange((nextAuthState, authData) => {
+            setAuthState(nextAuthState);
+            setUser(authData)
+        });
+    }, []);
+    console.log('a', authState, user)
+  return  authState === AuthState.SignedIn && user ? (
+      <App />
+    ) : (
+      <>
+        <AmplifyAuthenticator />
+        {
+          <TermsOfService
+          isOpen={!user}
+          onAgree={() => {
+            
+          }}
+          onDisagree={() => {
+            alert('To proceed on signin, you need to accept the Terms of Usage!')
+          }}
+        />
+        }
+      </>
+      // <AmplifyAuthenticator>
+      //   <AmplifySignIn
+      //     // headerText="My Custom Sign In Text"
+      //     slot="sign-in"
+      //   ></AmplifySignIn>
+      // </AmplifyAuthenticator>
+  )
+}
+// const AppWithAuth = withAuthenticator(App);
 
 // const AppContainer = () => {
 //   React.useEffect(() => {
