80ad75b29956066ba6d4fe88ef7ef9a49c4ecd69
sabaturgay <turgaysaba@outlook.com>
2021-09-08 13:45:18 +0300

    new aws-amplify

---
+++ amplify-old/#current-cloud-backend/function/queryhandler/src/clients/elasticsearch_client.py
@@ -0,0 +1,85 @@
+from elasticsearch import Elasticsearch, RequestsHttpConnection
+from requests_aws4auth import AWS4Auth
+import os
+import time
+
+
+class ElasticsearchClient:
+
+    def __init__(self, endpoint, index, max_hits=1, timeout=15, page_limit=1):
+
+        self.max_hits = max_hits
+        self.timeout = timeout
+        self.page_limit = page_limit
+        self.index=index
+
+        awsauth = AWS4Auth(
+            os.getenv('AWS_ACCESS_KEY_ID'), 
+            os.getenv('AWS_SECRET_ACCESS_KEY'), 
+            os.getenv('REGION'), 
+            'es', 
+            session_token=os.getenv('AWS_SESSION_TOKEN'))
+
+        self.es = Elasticsearch(
+            hosts = [{'host': endpoint, 'port': 443, 'use_ssl': True}],
+            http_auth = awsauth,
+            use_ssl = True,
+            verify_certs = True,
+            connection_class = RequestsHttpConnection,
+            timeout = self.timeout
+        )
+
+    
+    def execute(self, query, return_attributes):
+        """
+        retrieves given attributes for all items matching query
+
+        :param query: query dict in Elasticsearch query DSL
+        :param return_attributes: list of string attribute names to return
+        :return: list of item dicts containing attribute data
+        """
+        total_hits = []
+        #pit_id = es.open_point_in_time(index='caselaw4', keep_alive= f'{timeout/60}m')
+        start = time.time()
+        result = self.es.search(
+            index=[self.index],
+            body={
+                'size': self.max_hits,
+                'query': query,
+                #'pit': {'id': pit_id}
+                'sort': [{"_doc": "asc"}],
+                '_source': return_attributes
+            },
+            request_timeout = self.timeout
+        )
+        hits = result['hits']['hits']
+        total_hits.extend(hits)
+
+        counter = 1
+        while len(hits) > 0:
+            if counter == self.page_limit:
+                print(f'ELASTICSEARCH LIMIT REACHED - RETRIEVED {len(total_hits)} HITS')
+                break
+            #pit_id = result['pit_id']
+            result = self.es.search(
+                body={
+                    'size': self.max_hits,
+                    'query': query,
+                    #'pit': {'id': pit_id},
+                    'sort': [{"_doc": "asc"}],
+                    '_source': return_attributes,
+                    'search_after': hits[-1]['sort'],
+                    'track_total_hits': False
+                },
+                request_timeout = self.timeout
+            )
+            hits = result['hits']['hits']
+            total_hits.extend(hits)
+            counter += 1
+            #print(f'COUNTER: {counter}')
+        
+        #es.close_point_in_time(body={'id': pit_id})
+
+        #print('Duration es search:', time.time() - start)
+
+        return total_hits
