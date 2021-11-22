1dc574d6f82a8b213a905227b29e0ec7f8eec96e
Maxine Hanrieder <15159137+maxin-e@users.noreply.github.com>
2021-11-22 16:55:35 +0100

    add isResult flag to returned nodes

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,60 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "caseexplorerui": {
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "auth": {
+        "caseexploreruiauth": {
+            "HostedUIDomain": "string",
+            "OAuthMetadata": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "function": {
+        "datafetcher": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        },
+        "queryhandler": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        },
+        "test": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        },
+        "networkstatistics": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        },
+        "batchdatafetcher": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        },
+        "caseexploreruidynamodbaccess": {
+            "Arn": "string"
+        },
+        "caseexploreruiopensearchaccess": {
+            "Arn": "string"
+        },
+        "caseexploreruiutils": {
+            "Arn": "string"
+        }
+    }
+}
