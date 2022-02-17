3f007a466f3443870468d7b62174e5a07452461a
Maxine Hanrieder <15159137+maxin-e@users.noreply.github.com>
2022-02-17 11:18:17 +0100

    update layoutcalculatorrest

--- amplify/backend/function/layoutcalculatorrest/src/index.js
+++ amplify/backend/function/layoutcalculatorrest/src/index.js
@@ -18,5 +18,13 @@ exports.handler = async (event, context, callback) => {
   })
   console.log('result', result)
   // callback(null, result)
-  return result
+  return {
+    statusCode: 200,
+    //  Uncomment below to enable CORS requests
+    //  headers: {
+    //      "Access-Control-Allow-Origin": "*",
+    //      "Access-Control-Allow-Headers": "*"
+    //  }, 
+    body: JSON.stringify(result),
+  }
 }
