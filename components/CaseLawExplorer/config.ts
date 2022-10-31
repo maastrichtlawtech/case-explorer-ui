0ad700d2439e20c75762c584e45aa77fd7de93e0
Shashank Chakravarthy <shashank.chakravarthy@gmail.com>
2022-10-31 09:52:07 +0100

    Revert "New backend env setup"
    This reverts commit 1d3c6086dd3b7dedac45ebbf07389887d11b7319.

--- components/CaseLawExplorer/config.ts
+++ components/CaseLawExplorer/config.ts
@@ -3,7 +3,7 @@ import * as R from 'colay/ramda'
 
 const AWS_PROD_CONFIG = {
     "aws_project_region": "eu-central-1",
-    "aws_appsync_graphqlEndpoint": "https://zup25yq2azh75n33rwudtrtcoe.appsync-api.eu-central-1.amazonaws.com/graphql",
+    "aws_appsync_graphqlEndpoint": "https://mqnkmzj5ezd7vcdqxjp5lzdjj4.appsync-api.eu-central-1.amazonaws.com/graphql",
     "aws_appsync_region": "eu-central-1",
     "aws_appsync_authenticationType": "AMAZON_COGNITO_USER_POOLS",
     "aws_cloud_logic_custom": [
