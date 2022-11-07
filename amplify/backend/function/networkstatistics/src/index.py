af9879c7b652fe56d8ba5a415d9ebe1e2ea4cea4
Carlos <carloseam94@gmail.com>
2022-11-07 12:54:58 +0100

    centralities using networkit,graph-tool

--- amplify/backend/function/networkstatistics/src/index.py
+++ amplify/backend/function/networkstatistics/src/index.py
@@ -1,127 +1,175 @@
-from networkx import Graph, degree, degree_centrality, in_degree_centrality, out_degree_centrality, \
-    betweenness_centrality, closeness_centrality, hits, pagerank, NetworkXError
-from networkx.readwrite import json_graph
-from warnings import warn
-from community import community_louvain
-from time import time
-# taken from and modified: 
-# https://github.com/caselawanalytics/CaseLawAnalytics/blob/master/caselawnet/network_analysis.py
+import networkit as nk
+import networkx as nx
+import graph_tools as gt
 
-TEST = False
+# timer decorator
 
-def handler(event, context):
-    start = time()
 
-    #@TODO: remove after testing
-    if TEST:
-        from json import load
-        with open('edges.json') as f:
-            edges = load(f)
-        with open('nodes.json') as f:
-            nodes = load(f)
-    else:
-        network = event['arguments'].copy()
-        nodes = network['nodes']
-        edges = network['edges']
+def timer(fn):
+    from time import perf_counter
 
-    start_p = time()
+    def inner(*args, **kwargs):
+        start_time = perf_counter()
+        to_execute = fn(*args, **kwargs)
+        end_time = perf_counter()
+        execution_time = end_time - start_time
+        print('{0} took {1:.8f}s to execute'.format(
+            fn.__name__, execution_time))
+        return to_execute
+
+    return inner
+
+# return the same network in 3 different formats (networkx, networkit and graph_tools)
+
+
+@timer
+def get_networks(nodes, edges):
+    nxG = nx.readwrite.json_graph.node_link_graph(
+        {'nodes': nodes, 'links': edges}, directed=True, multigraph=False)
+    nkG = nk.nxadapter.nk2nx(nxG)
+    gtG = ()
+    return nxG, nkG, gtG
+
+# networkit centralities
+
+
+@timer
+def get_outdegree_centrality(G):
+    return nk.centrality.ranking(
+        G, algorithm=nk.centrality.DegreeCentrality, normalized=True)
+
+
+@timer
+def get_indegree_centrality(G):
+    idg_centrality = nk.centrality.DegreeCentrality(
+        G, outDeg=False, normalized=True)
+    idg_centrality.run()
+    return idg_centrality.ranking()
+
+
+@timer
+def get_betweenness_centrality(G):
+    return nk.centrality.ranking(
+        G, algorithm=nk.centrality.ApproxBetweenness, normalized=True)
+
+
+@timer
+def get_closeness_centrality(G):
+    return nk.centrality.ranking(
+        G, algorithm=nk.centrality.ApproxCloseness, normalized=True)
+
+
+@timer
+def get_pagerank_centrality(G):
+    return nk.centrality.ranking(
+        G, algorithm=nk.centrality.PageRank, normalized=True)
+
+
+@timer
+def get_communities_centrality(G):
+    uG = nk.graphtools.toUndirected(G)
+    return nk.community.detectCommunities(G, algo=nk.community.PLM(uG, True))
+
+# networkx centralities and other info
+
+
+@timer
+def get_degree_centrality(G):
+    return nx.degree_centrality(G)
+
+
+@timer
+def get_degree(G):
+    return nx.degree(G)
+
+
+@timer
+def get_indegree(G):
+    return G.in_degree()
+
+
+@timer
+def get_outdegree(G):
+    return G.out_degree()
+
+# graph-tools centralities
+
+
+@timer
+def get_hits_centrality(G):
+    return [0], [0]
+
+# other computations
+
+
+@timer
+def sort_by_date(nodes):
+    def derive_date(
+        k): return k['data']['date_decision'] if 'date_decision' in k['data'] and k['data']['date_decision'] != '' else '1900-01-01'
+    nodes.sort(key=derive_date, reverse=True)
+
+# this one includes the computation of the relative indegree centrality
+
+
+@timer
+def create_response(nodes, degrees, in_degrees, out_degrees, degree_centralities, in_degree_centralities,
+                    out_degree_centralities, page_ranks, authorities, hubs, betweenness_centralities, closeness_centralities, partition):
     statistics = dict()
-    if len(nodes) == 0:
-        return statistics
-    start_p = time()
-    graph = get_network(nodes, edges)
-    print(f'get network: took {time()-start_p} s.')
-    start_p = time()
-    partition = community_louvain.best_partition(Graph(graph))
-    print(f'get partition: took {time()-start_p} s.')
-    start_p = time()
-    degrees = degree(graph)
-    print(f'get degree: took {time()-start_p} s.')
-    if max(dict(degrees).values()) > 0:
-        start_p = time()
-        hubs, authorities = get_hits(graph)
-        print(f'get hubs & authorities: took {time()-start_p} s.')
-        start_p = time()
-        in_degrees = graph.in_degree()
-        print(f'get in degree: took {time()-start_p} s.')
-        start_p = time()
-        out_degrees = graph.out_degree()
-        print(f'get out degree: took {time()-start_p} s.')
-        start_p = time()
-        degree_centralities = degree_centrality(graph)
-        print(f'get degree centrality: took {time()-start_p} s.')
-        start_p = time()
-        in_degree_centralities = in_degree_centrality(graph)
-        print(f'get in degree centrality: took {time()-start_p} s.')
-        start_p = time()
-        out_degree_centralities = out_degree_centrality(graph)
-        print(f'get out degree centrality: took {time()-start_p} s.')
-        start_p = time()
-        if len(nodes) < 2500:
-            betweenness_centralities = betweenness_centrality(graph)
-        else:
-            betweenness_centralities = betweenness_centrality(graph, k=2500)
-        print(f'get betweenness centrality: took {time()-start_p} s.')
-        start_p = time()
-        closeness_centralities = closeness_centrality(graph)
-        print(f'get closeness centrality: took {time()-start_p} s.')
-        start_p = time()
-        page_ranks = get_pagerank(graph)
-        print(f'get page rank: took {time()-start_p} s.')
-
-        start_p = time()
-        # for relative in-degree we sort on date
-        derive_date = lambda k: k['data']['date_decision'] if 'date_decision' in k['data'] and k['data']['date_decision'] != '' else '1900-01-01' # @ TODO: which default date?
-        nodes.sort(key=derive_date, reverse=True)
-        for i, node in enumerate(nodes):
-            node_id = node['id']
-            statistics[node_id] = {
-                'degree': degrees[node_id],
-                'in-degree': in_degrees[node_id],
-                'out-degree': out_degrees[node_id],
-                'degree centrality': degree_centralities[node_id],
-                'in-degree centrality': in_degree_centralities[node_id],
-                'out-degree centrality': out_degree_centralities[node_id],
-                'relative in-degree': in_degrees[node_id] / float(max(i, 1)),
-                'pageRank': page_ranks[node_id],
-                'authorities': authorities[node_id],
-                'hubs': hubs[node_id],
-                'betweenness centrality': betweenness_centralities[node_id],
-                'closeness centrality': closeness_centralities[node_id],
-                'community': partition[node_id],
-                'year': int(node['id'].split(':')[3])
-            }
-        print(f'STATS: add to nodes took: {time() - start_p} s.')
-    print(f'STATS: compute network took: {time() - start} s.')
-    
-    if TEST:
-        return len(statistics)
-    
+    for i, node in enumerate(nodes):
+        node_id = node['id']
+        statistics[node_id] = {
+            'degree': degrees[node_id],
+            'in-degree': in_degrees[node_id],
+            'out-degree': out_degrees[node_id],
+            'degree centrality': degree_centralities[node_id],
+            'in-degree centrality': in_degree_centralities[node_id],
+            'out-degree centrality': out_degree_centralities[node_id],
+            'relative in-degree': in_degrees[node_id] / float(max(i, 1)),
+            'pageRank': page_ranks[node_id],
+            'authorities': authorities[node_id],
+            'hubs': hubs[node_id],
+            'betweenness centrality': betweenness_centralities[node_id],
+            'closeness centrality': closeness_centralities[node_id],
+            'community': partition[node_id],
+            'year': int(node['id'].split(':')[3])
+        }
     return statistics
 
-def get_network(nodes, edges):
-    graph = json_graph.node_link_graph({'nodes': nodes, 'links': edges}, directed=True, multigraph=False)
-    return graph
+# main
 
-def get_hits(graph, max_iter=10000):
-    try:
-        hubs, authorities = hits(graph, max_iter=max_iter)
-        return hubs, authorities
-    except NetworkXError:
-        # It is possible that the HITS algorithm doesn't converge
-        warn('HITS algorithm did not converge!',
-                      Warning)
-        h = dict.fromkeys(graph, 1.0 / graph.number_of_nodes())
-        hubs, authorities = h, h
-        return hubs, authorities
 
-def get_pagerank(graph, max_iter=10000):
-    try:
-        g_pagerank = pagerank(graph, max_iter=max_iter)
-        return g_pagerank
-    except NetworkXError:
-        # It is possible that the pagerank algorithm doesn't converge
-        warn('PageRank algorithm did not converge!',
-                      Warning)
-        p = dict.fromkeys(graph, 1.0 / graph.number_of_nodes())
-        return p
+@timer
+def handler(event, context):
+
+    network = event['arguments'].copy()
+    nodes = network['nodes']
+    edges = network['edges']
+
+    if len(nodes) == 0 or len(edges) == 0:
+        return dict()
+
+    nxG, nkG, gtG = get_networks(nodes, edges)
+
+    # compute networx centralities
+    degrees = get_degree(nxG)
+    in_degrees = get_indegree(nxG)
+    out_degrees = get_outdegree(nxG)
+    degree_centralities = get_degree_centrality(nxG)
+
+    # compute networkit centralities
+    partition = get_communities_centrality(nkG)
+    in_degree_centralities = get_indegree_centrality(nkG)
+    out_degree_centralities = get_outdegree_centrality(nkG)
+    betweenness_centralities = get_betweenness_centrality(nkG)
+    closeness_centralities = get_closeness_centrality(nkG)
+    page_ranks = get_pagerank_centrality(nkG)
+
+    # compute graph-tools centralities
+    hubs, authorities = get_hits_centrality(gtG)
+
+    # for relative in-degree we sort on date
+    sort_by_date(nodes)
+
+    statistics = create_response(nodes, degrees, in_degrees, out_degrees, degree_centralities, in_degree_centralities,
+                                 out_degree_centralities, page_ranks, authorities, hubs, betweenness_centralities, closeness_centralities, partition)
+    return statistics
