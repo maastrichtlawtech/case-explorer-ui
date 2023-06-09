332bc8c7952704b87cd3099523c8be8582ad8f47
Merijn Verstraaten <m.verstraaten@esciencecenter.nl>
2023-06-09 16:44:05 +0200

    Add a simple timer class for various timers.

--- docker/networkstats/app.py
+++ docker/networkstats/app.py
@@ -2,8 +2,26 @@
 from time import perf_counter
 import networkit as nk
 
+class Timer:
+    "Simple timer class for reporting the time taken by a block of code."
+
+    def __init__(self, name):
+        self.name = name
+        self.start_time = None
+
+    def __enter__(self):
+        "Starts the timer upon entering a with-block."
+        self.start_time = perf_counter()
+
+    def __exit__(self, exc_type, exc_val, tb):
+        "Ends the timer and reports time taken upon exiting a with-block."
+        end_time = perf_counter()
+        execution_time = end_time - self.start_time
+        print(f'{self.name} took {execution_time:.8f}s to execute')
+
 # timer decorator
 def timer(fn):
+    "Decorator for wrapping function calls with a Timer."
     def inner(*args, **kwargs):
         start_time = perf_counter()
         to_execute = fn(*args, **kwargs)
